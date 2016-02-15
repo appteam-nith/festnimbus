709fcc6651cf10b5163cd0483de2f7ffae47ec32
Ramola <sramola5@gmail.com>
2016-02-16 01:21:10 +0530

    Filled the detail of each team and changed the color

--- app/src/main/java/com/appteam/nimbus/DepartmentFragment.java
+++ app/src/main/java/com/appteam/nimbus/DepartmentFragment.java
@@ -2,10 +2,13 @@ package com.appteam.nimbus;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +21,7 @@ public class DepartmentFragment extends Fragment {
     private static final String DETAIl="detail";
     private static final String IMAGE_ID = "image";
 
+    private  String USER_NAME,USER_DETAIL,USER_IMAGE_ID;
     public DepartmentFragment() {
         // Required empty public constructor
     }
@@ -27,14 +31,29 @@ public  static Fragment newInstance(String departmentname,String detail,int imag
     bundle.putString(NAME,departmentname);
     bundle.putString(DETAIl,detail);
     bundle.putInt(IMAGE_ID,image_id);
+    fragment.setArguments(bundle);
     return  fragment;
 }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        USER_NAME=getArguments().getString(NAME,"USERNAME");
+        USER_DETAIL=getArguments().getString(DETAIl,"USER DETAIl");
+        USER_IMAGE_ID=getArguments().getString(USER_IMAGE_ID);
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_department, container, false);
+        View v=inflater.inflate(R.layout.fragment_department, container, false);
+        TextView name_textView= (TextView) v.findViewById(R.id.team_name_department);
+        TextView content_textView= (TextView) v.findViewById(R.id.team_detail_department);
+        ImageView imageView= (ImageView) v.findViewById(R.id.image_department);
+        name_textView.setText(USER_NAME);
+        content_textView.setText(USER_DETAIL);
+        return v;
     }
 
 }
